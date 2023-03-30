775947f1a143d8ee9b0e76a29b3fe098b6eba77d
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-03-30 23:42:12 +0300

    feat: added test dates that have already passed

--- js/script.js
+++ js/script.js
@@ -34,16 +34,28 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+
     //Timer
 
 
-    const deadline = '2023-04-01';
+    const deadline = '2023-01-01';
     function getTimeRemaining(endtime){
-        const t = Date.parse(endtime) - Date.parse(new Date()),
+
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
             hours = Math.floor((t / (1000 * 60 * 60)) % 24),
             minutes = Math.floor((t / 1000 / 60) % 60),
             seconds = Math.floor((t / 1000 ) % 60);
+        }
+
 
         return {
             'total': t,
@@ -77,7 +89,7 @@ window.addEventListener('DOMContentLoaded', () => {
         function updateClock() {
             const t = getTimeRemaining(endtime);
 
-            hours.innerHTML = getZero(t.hours);
+            days.innerHTML = getZero(t.days);
             hours.innerHTML = getZero(t.hours);
             minutes.innerHTML = getZero(t.minutes);
             seconds.innerHTML = getZero(t.seconds);
