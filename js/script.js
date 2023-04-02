9f81a40cf39f8afac22a0d2823bfb7d57cadd3aa
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-04-02 20:44:49 +0300

    refactor: added rest-operator for className in generated cards

--- js/script.js
+++ js/script.js
@@ -153,12 +153,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // use classes for cards
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes ) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -170,8 +171,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if(this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element)
+            }else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
             element.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -180,7 +186,6 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-        </div>
         `;
         this.parent.append(element)
         }
@@ -202,7 +207,7 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         21,
-        '.menu .container'
+        '.menu .container '
     ).render();
     new MenuCard(
         "img/tabs/post.jpg",
