e18ad9a0a21763221e50a641a35357f3c3c14d1f
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-04-23 01:27:19 +0300

    refactor: created new func with shortcut reEx

--- js/script.js
+++ js/script.js
@@ -38,7 +38,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Timer
 
-
     const deadline = '2023-04-24';
     /* const deadline = new Date().toString().slice(0, 10); */
     function getTimeRemaining(endtime){
@@ -403,12 +402,15 @@ window.addEventListener('DOMContentLoaded', () => {
             dots.push(dot);
     }
     
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
 
     next.addEventListener('click', () => {
-        if(offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+        if(offset == deleteNotDigits(width) * (slides.length - 1)){
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -430,9 +432,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if(offset = 0){
-            offset += +width.slice(0, width.length - 2) * (slides.length - 1)
+            offset += deleteNotDigits(width) * (slides.length - 1)
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -457,7 +459,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
             
